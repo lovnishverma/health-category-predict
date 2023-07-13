a1516ab57fe9d3fc79f06e7e70c4d59ac0dc9adb
Glitch (glitch-hello-website) <none>
2023-07-13 11:51:31 +0000

    🚄🚝 Checkpoint ./templates/index.html:59818739/80 ./requirements.txt:59818739/2 ./app.py:59818739/8

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LogisticRegression
 app=Flask(__name__)
 
 @app.route('/')
-def iris():
+def home():
   return render_template("index.html")
 
 @app.route('/bmi', methods=["POST"])
