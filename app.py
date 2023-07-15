541fc6edb0203682a939f9785db30f3cae8f5095
Glitch (health-predict) <none>
2023-07-15 09:14:43 +0000

    🍶🏹 Checkpoint ./app.py:57629255/1666 ./templates/index.html:57629255/10503 ./requirements.txt:57629255/4

--- app.py
+++ app.py
@@ -1,37 +1,44 @@
 from flask import *
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-  return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route('/bmi', methods=["POST"])
 def page():
-  gen=eval(request.form.get("gen"))
-  weight=eval(request.form.get("weight"))
-  height=eval(request.form.get("height"))
-  bmi=eval(request.form.get("bmi"))
-  
-  url="BMI_Data.csv"
-  
-  data=pd.read_csv(url)
-  health=data.values
-  
-  #Split
-  x=health[:,:4]
-  y=health[:,-1]
-  
-  model=LogisticRegression()
-  model.fit(x,y)
-  
-  arr=model.predict([[gen,weight,height,bmi]])
+    gen = eval(request.form.get("gen"))
+    weight = eval(request.form.get("weight"))
+    height = eval(request.form.get("height"))
+    bmi = eval(request.form.get("bmi"))
 
-  return render_template("index.html", data=str(arr[0]))
+    url = "BMI_Data.csv"
+
+    data = pd.read_csv(url)
+    health = data.values
+
+    # Split the data
+    x = health[:, :4]
+    y = health[:, -1]
+
+    model = RandomForestClassifier()
+    model.fit(x, y)
+
+    arr = model.predict([[gen, weight, height, bmi]])
+
+    # Calculate accuracy
+    accuracy = accuracy_score(y, model.predict(x))
+    accuracy_percentage = accuracy * 100
+
+    return render_template("index.html", data=str(arr[0]), accuracy=accuracy_percentage)
 
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
